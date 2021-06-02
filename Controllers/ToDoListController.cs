a7b614787de0251655e344e95344c0afccef7a23
Tobias-GetAcademy <69505587+Tobias-GetAcademy@users.noreply.github.com>
2021-06-02 13:58:24 +0200

    x

--- Controllers/ToDoListController.cs
+++ Controllers/ToDoListController.cs
@@ -14,38 +14,23 @@ namespace ToDoAPI.Controllers
     {
         // CRUD - Create Read Update Delete
 
-        public List<ToDo> ToDoList { get; set; } = new List<ToDo>
-        {
-            new ToDo() {Id = 1, TaskName = "Vaske", TaskDate = "20.03.2021", Person = "Tobias"},
-            new ToDo() {Id = 2, TaskName = "lage", TaskDate = "20.04.2021", Person = "Pål"},
-            new ToDo() {Id = 3, TaskName = "Klippe gress", TaskDate = "Mandag", Person = "Per"},
-            new ToDo() {Id = 4, TaskName = "rydde", TaskDate = "Tirsdag", Person = "Olsen"},
-            new ToDo() {Id = 5, TaskName = "handle", TaskDate = "Torsdag", Person = "Ola"},
-            new ToDo() {Id = 6, TaskName = "sove", TaskDate = "Fredag", Person = "Askeladen"}
-        };
-
-        public ToDoListController(List<ToDo> toDo)
-        {
-            ToDoList = toDo;
-        }
-
         [HttpGet("{id}")]
         public async Task<ToDo> GetOne(int id)
         {
             // Where TaskName == "Vaske"
-            return await Task.Run(() => ToDoList.FirstOrDefault(x => x.Id == id));
+            return await Task.Run(() => ToDo.ToDoList.FirstOrDefault(x => x.Id == id));
         }
 
         [HttpGet]
         public async Task<IEnumerable<ToDo>> GetMany()
         {
-            return await Task.Run(() => ToDoList);
+            return await Task.Run(() => ToDo.ToDoList);
         }
         
         [HttpPost]
         public async Task<int> Create(ToDo toDo)
         {
-            ToDoList.Add(toDo);
+            ToDo.AddToDoTask(toDo);
             return 1;
         }
 
--- Model/ToDo.cs
+++ Model/ToDo.cs
@@ -11,5 +11,22 @@ namespace ToDoAPI.Model
         public string TaskName { get; set; }
         public string TaskDate { get; set; }
         public string Person { get; set; }
+
+        public static List<ToDo> ToDoList = new List<ToDo>
+        {
+            new ToDo() {Id = 1, TaskName = "Vaske", TaskDate = "20.03.2021", Person = "Tobias"},
+            new ToDo() {Id = 2, TaskName = "lage", TaskDate = "20.04.2021", Person = "Pål"},
+            new ToDo() {Id = 3, TaskName = "Klippe gress", TaskDate = "Mandag", Person = "Per"},
+            new ToDo() {Id = 4, TaskName = "rydde", TaskDate = "Tirsdag", Person = "Olsen"},
+            new ToDo() {Id = 5, TaskName = "handle", TaskDate = "Torsdag", Person = "Ola"},
+            new ToDo() {Id = 6, TaskName = "sove", TaskDate = "Fredag", Person = "Askeladen"}
+        };
+
+
+        public static ToDo AddToDoTask(ToDo task)
+        {
+            ToDoList.Add(task);
+            return task;
+        }
     }
 }
