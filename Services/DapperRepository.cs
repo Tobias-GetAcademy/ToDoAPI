c46459ee48ddbb4ea669271a45676c7218a6489c
Tobias-GetAcademy <69505587+Tobias-GetAcademy@users.noreply.github.com>
2021-06-09 13:58:24 +0200

    #2

--- Services/DapperRepository.cs
+++ Services/DapperRepository.cs
@@ -1,50 +1,50 @@
-﻿//using Dapper;
-//using Microsoft.Extensions.Configuration;
-//using System;
-//using System.Collections.Generic;
-//using System.Data;
-//using System.Data.SqlClient;
-//using System.Linq;
-//using System.Threading.Tasks;
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
 
-//namespace ToDoAPI.Services
-//{
-//    public class DapperRepository
-//    {
-//        public class DapperRepository : IDapperRepository
-//        {
-//            private readonly IConfiguration _configuration;
-//            public DapperRepository(IConfiguration configuration)
-//            {
-//                _configuration = configuration;
-//            }
-//            public List<T> GetAll<T>(string query, DynamicParameters sp_params, CommandType commandType = CommandType.StoredProcedure)
-//            {
-//                using IDbConnection db = new SqlConnection(_configuration.GetConnectionString("default"));
-//                return db.Query<T>(query, sp_params, commandType: commandType).ToList();
-//            }
-//            public T execute_sp<T>(string query, DynamicParameters sp_params, CommandType commandType = CommandType.StoredProcedure)
-//            {
-//                T result;
-//                using (IDbConnection dbConnection = new SqlConnection(_configuration.GetConnectionString("default")))
-//                {
-//                    if (dbConnection.State == ConnectionState.Closed)
-//                        dbConnection.Open();
-//                    using var transaction = dbConnection.BeginTransaction();
-//                    try
-//                    {
-//                        dbConnection.Query<T>(query, sp_params, commandType: commandType, transaction: transaction);
-//                        result = sp_params.Get<T>("retVal"); //get output parameter value
-//                        transaction.Commit();
-//                    }
-//                    catch (Exception ex)
-//                    {
-//                        transaction.Rollback();
-//                        throw ex;
-//                    }
-//                };
-//                return result;
-//            }
-//        }
-//    }
-//}
+namespace ToDoAPI.Services
+{
+    public class DapperRepository
+    {
+        public class DapperRepository : IDapperRepository
+        {
+            private readonly IConfiguration _configuration;
+            public DapperRepository(IConfiguration configuration)
+            {
+                _configuration = configuration;
+            }
+            public List<T> GetAll<T>(string query, DynamicParameters sp_params, CommandType commandType = CommandType.StoredProcedure)
+            {
+                using IDbConnection db = new SqlConnection(_configuration.GetConnectionString("default"));
+                return db.Query<T>(query, sp_params, commandType: commandType).ToList();
+            }
+            public T execute_sp<T>(string query, DynamicParameters sp_params, CommandType commandType = CommandType.StoredProcedure)
+            {
+                T result;
+                using (IDbConnection dbConnection = new SqlConnection(_configuration.GetConnectionString("default")))
+                {
+                    if (dbConnection.State == ConnectionState.Closed)
+                        dbConnection.Open();
+                    using var transaction = dbConnection.BeginTransaction();
+                    try
+                    {
+                        dbConnection.Query<T>(query, sp_params, commandType: commandType, transaction: transaction);
+                        result = sp_params.Get<T>("retVal"); //get output parameter value
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                };
+                return result;
+            }
+        }
+    }
+}
--- Services/IDapperRepository.cs
+++ Services/IDapperRepository.cs
@@ -1,16 +1,16 @@
-﻿//using Dapper;
-//using System;
-//using System.Collections.Generic;
-//using System.Data;
-//using System.Linq;
-//using System.Threading.Tasks;
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
 
-//namespace ToDoAPI.Services
-//{
-//    public interface IDapperRepository
-//    {
-//        T execute_sp<T>(string query, DynamicParameters sp_params, CommandType commandType = CommandType.StoredProcedure);
-//        List<T> GetAll<T>(string query, DynamicParameters sp_params, CommandType commandType = CommandType.StoredProcedure);
-//    }
-//}
+namespace ToDoAPI.Services
+{
+    public interface IDapperRepository
+    {
+        T execute_sp<T>(string query, DynamicParameters sp_params, CommandType commandType = CommandType.StoredProcedure);
+        List<T> GetAll<T>(string query, DynamicParameters sp_params, CommandType commandType = CommandType.StoredProcedure);
+    }
+}
 
